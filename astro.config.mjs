5739f763b16c480f45144fa01416ac3a4b1e030c
xeusteerapat <xeus085@gmail.com>
2025-10-21 13:08:18 +0700

    🐛 Fix weird css

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    inlineStylesheets: 'never'
+  },
+  vite: {
+    css: {
+      devSourcemap: true
+    }
+  }
+});
